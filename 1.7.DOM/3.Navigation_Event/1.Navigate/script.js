5f2dd5674fc7ea63292fe8ebbf2b92b25c3ab3f0
Hugo Goorickx <hugo@goorickx.be>
2022-12-05 11:52:43 +0100

    nouveau travail sur les events

--- 1.7.DOM/3.Navigation_Event/1.Navigate/script.js
+++ 1.7.DOM/3.Navigation_Event/1.Navigate/script.js
@@ -1,2 +1,33 @@
 /*
 */
+
+//first exercice
+    //select two li
+const place_ol = document.querySelector('ol');
+let end = place_ol.children[place_ol.children.length - 1];
+let start = place_ol.children[0];
+
+    //move end before start
+place_ol.insertBefore(end, start);
+
+//second exercise
+    //select two sections
+const all_sect = document.querySelectorAll('section');
+const sect2 = all_sect[1];
+const sect3 = all_sect[2];
+
+    //init two h2 to switch
+let sect2_h2 = sect2.querySelector('h2');
+let sect3_h2 = sect3.querySelector('h2');
+
+    //make deep copy
+var sect2_h2_cop = sect2_h2.cloneNode(true);
+var sect3_h2_cop = sect3_h2.cloneNode(true);
+
+    //switch the nodes
+sect3_h2.parentNode.replaceChild(sect2_h2_cop, sect3_h2);
+sect2_h2.parentNode.replaceChild(sect3_h2_cop, sect2_h2);
+
+//third exercise
+    //remove section 3
+sect3.remove();
---
+++ 1.7.DOM/3.Navigation_Event/2.Events/script.js
@@ -0,0 +1,107 @@
+const _initTime = Date.now()
+
+const getElapsedTime = () =>
+{
+    return Number((Date.now() - _initTime) / 1000).toFixed(2) + 's'
+}
+
+const clickOnSquare = (e) =>
+{
+    console.log(e.target.classList[1])
+    console.log(getElapsedTime())
+}
+
+const actionSquares = document.querySelectorAll('.actionsquare')
+for (let actionSquare of actionSquares)
+{
+    actionSquare.addEventListener('click', clickOnSquare)
+}
+
+//gen a random value between 0 and x
+function rand(x)
+{
+    return (Math.floor(Math.random() * x) + 1);
+}
+
+//gen the background
+function bakcground(input)
+{
+    let r = rand(255);
+    let g = rand(255);
+    let b = rand(255);
+    input.style.background = "rgb(" + r + ", " + g + ", " + b + ")";
+}
+
+//write the log
+function log_input(txt)
+{
+    let tmp_bal;
+    let tmp_txt;
+    tmp_bal = document.createElement('li');
+    tmp_txt = document.createTextNode("[" + getElapsedTime() + "] " + txt);
+    tmp_bal.appendChild(tmp_txt);
+    ul_start.appendChild(tmp_bal);
+    ul_start.appendChild;
+}
+
+//function for input
+function inputSpace(e)
+{
+    console.log(e);
+
+
+    //if spacebar
+    if (e.charCode == 32)
+    {
+        bakcground(body);
+        log_input("Push on space bar");
+    }
+    else if (e.charCode == 108)
+    {
+        while (ul_start.querySelectorAll('li').length)
+            ul_start.querySelector('li').remove();
+    }
+    else if (e.charCode == 115)
+    {
+        while (add_square_start.querySelectorAll('.displayedsquare').length)
+            add_square_start.querySelectorAll('.displayedsquare')[0].remove();
+    }
+}
+
+//Function to input the square
+function inputSquare(color)
+{
+    let tmp = document.createElement('div');
+    tmp.classList.add('displayedsquare');
+    tmp.classList.add(color);
+    add_square_start.appendChild(tmp);
+    log_input("Create a new " + color + " square");
+    tmp.addEventListener('click', function() {inputSquareGen(color);});
+}
+
+//setup the alert box with the color
+function inputSquareGen(color)
+{
+    window.alert(color)
+}
+
+//put body in var
+const body = document.body;
+
+//select the ul for the logs
+const ul_start = document.querySelector('ul');
+
+//select the new squares zone
+const add_square_start = document.querySelector('.displayedsquare-wrapper');
+
+//select squares
+const square_start = document.querySelector('.actionsquare-wrapper');
+const square_green = square_start.querySelector(".green");
+const square_violet = square_start.querySelector(".violet");
+const square_orange = square_start.querySelector(".orange");
+
+//setup the detectors for addEventListener
+square_green.addEventListener('click', function() {inputSquare('green');});
+square_violet.addEventListener('click', function() {inputSquare('violet');});
+square_orange.addEventListener('click', function() {inputSquare('orange');});
+body.addEventListener('keypress', inputSpace);

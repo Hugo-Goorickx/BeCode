a25a8ec5a34b47af5165f4dd86f1084ca4ba5ff3
Hugo Goorickx <hugo@goorickx.be>
2022-12-19 11:10:52 +0100

    Ajout des bonus de l'assynchrone

--- 2.4.Assynchrone/ex2/ex2.js
+++ 2.4.Assynchrone/ex2/ex2.js
@@ -1,15 +1,22 @@
 function loadAPI()
 {
-    let input = document.getElementById("name").value; 
-
-    const fetchName = (name) => fetch("https://api.agify.io/?name=" + name);
+    let inputName = document.getElementById("name").value; 
+    let inputCountry = document.getElementById("country").value; 
+    let tmp = [];
+    const fetchName = (name, country) => fetch("https://api.agify.io/?name=" + name + "&country_id=" + country);
     
-    fetchName(input)
+    fetchName(inputName, inputCountry)
         .then((response) => response.json())
         .then((json) => {
             let newDiv = document.createElement("div");
-            newDiv.appendChild(document.createTextNode(input + " a " + json.age + " et compte " + json.count));
+            newDiv.appendChild(document.createTextNode(inputName + " a " + json.age + "ans et compte " + json.count));
             document.body.appendChild(newDiv);
+            tmp = JSON.parse(localStorage.getItem("Save"));
+            if (!tmp)
+                tmp = [];
+            tmp.push(json);
+            localStorage.setItem("Save", JSON.stringify(tmp));
+            console.log(tmp);
         })
         .catch((error) => {
             console.log("There was an error!", error);
@@ -17,3 +24,14 @@ function loadAPI()
 }
 
 document.getElementById("button").addEventListener("click", loadAPI);
+
+let tmp = JSON.parse(localStorage.getItem("Save"));
+if (tmp)
+{
+    for (let elem of tmp)
+    {
+        let newDiv = document.createElement("div");
+        newDiv.appendChild(document.createTextNode(elem.name + " a " + elem.age + "ans et compte " + elem.count));
+        document.body.appendChild(newDiv);
+    }
+}
